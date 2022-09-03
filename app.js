ebfed24fe1662c2ade4824b7dcdd800fc9492e7d
Miguel <miguel.castillo-melendez81@bcmail.cuny.edu>
2022-09-03 15:26:12 -0400

    lesson 24: type aliases

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-function combine(input1, input2) {
+function combine(input1, // union types
+input2, // union types
+resultType //'as-number' | 'as-text' // literal types
+) {
     var result;
     if (typeof input1 === 'number' && typeof input2 === 'number') {
         result = input1 + input2;
@@ -6,9 +9,17 @@ function combine(input1, input2) {
     else {
         result = input1.toString() + input2.toString();
     }
+    if (resultType === 'as-number') {
+        return +result;
+    }
+    else {
+        return result.toString();
+    }
     return result;
 }
-var combinedAges = combine(30, 26);
+var combinedAges = combine(30, 26, 'as-number');
 console.log(combinedAges);
-var combinedNames = combine('Max', 'Anna');
+var combinedStringAges = combine(30, 26, 'as-number');
+console.log(combinedStringAges);
+var combinedNames = combine('Max', 'Anna', 'as-text');
 console.log(combinedNames);
