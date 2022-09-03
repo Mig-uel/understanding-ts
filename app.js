8448f1f762228f013e7537832d678176f18ea4ae
Miguel <miguel.castillo-melendez81@bcmail.cuny.edu>
2022-09-02 22:15:32 -0400

    lesson 18: array types..

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
+// const person: {
+//   name: string
+//   age: number
+// } = {
 var person = {
     name: 'Miguel',
-    age: 23
+    age: 23,
+    hobbies: ['Sports', 'Cooking']
 };
-console.log(person.age);
-console.log(person.name);
+var favoriteActivities;
+favoriteActivities = ['Miguel'];
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
--- app.ts
+++ app.ts
@@ -1,10 +1,17 @@
-const person: {
-  name: string
-  age: number
-} = {
+// const person: {
+//   name: string
+//   age: number
+// } = {
+
+const person = {
   name: 'Miguel',
   age: 23,
+  hobbies: ['Sports', 'Cooking'],
 }
 
-console.log(person.age)
-console.log(person.name)
+let favoriteActivities: string[]
+favoriteActivities = ['Miguel']
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase())
+}
