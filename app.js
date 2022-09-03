f59d1ce89f6804dcdeac038fea08d0e80f123b71
Miguel <miguel.castillo-melendez81@bcmail.cuny.edu>
2022-09-02 22:22:30 -0400

    lesson 19: tuples

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
+"use strict";
 // const person: {
 //   name: string
 //   age: number
 // } = {
+exports.__esModule = true;
 var person = {
     name: 'Miguel',
     age: 23,
-    hobbies: ['Sports', 'Cooking']
+    hobbies: ['Sports', 'Cooking'],
+    role: [2, 'author']
 };
 var favoriteActivities;
 favoriteActivities = ['Miguel'];
@@ -13,3 +16,4 @@ for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
 }
+// tuples: fixed length and type array
--- app.ts
+++ app.ts
@@ -3,10 +3,18 @@
 //   age: number
 // } = {
 
-const person = {
+import { TupleType } from 'typescript'
+
+const person: {
+  name: string
+  age: number
+  hobbies: string[]
+  role: [number, string] // tuple type
+} = {
   name: 'Miguel',
   age: 23,
   hobbies: ['Sports', 'Cooking'],
+  role: [2, 'author'],
 }
 
 let favoriteActivities: string[]
@@ -15,3 +23,5 @@ favoriteActivities = ['Miguel']
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase())
 }
+
+// tuples: fixed length and type array
