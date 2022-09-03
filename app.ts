ecdd16180ee860898155cd8372fa5ed6622d7b3a
Miguel <miguel.castillo-melendez81@bcmail.cuny.edu>
2022-09-03 15:57:27 -0400

    lesson 28: function types and callbacks

--- app.ts
+++ app.ts
@@ -8,6 +8,13 @@ function printResult(num: number): void {
   console.log('Result: ' + num)
 }
 
+function addAndHandle(n1: number, n2: number, callback: (a: number) => void) {
+  // cb is a function that takes a number and returns nothing/void
+  const result = n1 + n2
+
+  callback(result)
+}
+
 printResult(add(5, 12))
 
 let someValue: undefined // undefined is a valid type
@@ -16,3 +23,7 @@ let combinedValues: (a: number, b: number) => number // function type and return
 combinedValues = add
 
 console.log(combinedValues(8, 8))
+
+addAndHandle(10, 20, (result) => {
+  console.log(result)
+})
