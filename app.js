f64f6c73a0f3b6b606d5f8da5fd522566b49f4a2
Miguel <miguel.castillo-melendez81@bcmail.cuny.edu>
2022-09-02 22:08:42 -0400

    lesson 16: object types.

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
-function add(n1, n2, printResult, phrase) {
-    // if (typeof n1 !== 'number' || typeof n2 !== 'number')
-    //   throw new Error('Incorrect input!')
-    printResult ? console.log(phrase, n1 + n2) : n1 + n2;
-}
-var number1 = 5;
-var number2 = 2.8;
-var printResult = true;
-var phrase = 'Result is:';
-add(number1, number2, printResult, phrase);
+var person = {
+    name: 'Miguel',
+    age: 23
+};
+console.log(person.age);
+console.log(person.name);
