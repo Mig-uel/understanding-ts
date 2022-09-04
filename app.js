a8e80916a4bf77a01eb1de6431fd806e9172ebe6
Miguel <miguel.castillo-melendez81@bcmail.cuny.edu>
2022-09-03 23:14:50 -0400

    lesson 29: never type.

--- app.js
+++ app.js
@@ -4,3 +4,8 @@ userInput = 5;
 userInput = 'Max';
 if (typeof userInput === 'string')
     username = userInput;
+function error(message, code) {
+    // never returns anything, crash break your script
+    throw { message: message, code: code };
+}
+error('An error occurred!', 500);
--- app.ts
+++ app.ts
@@ -5,3 +5,10 @@ userInput = 5
 userInput = 'Max'
 
 if (typeof userInput === 'string') username = userInput
+
+function error(message: string, code: number): never {
+  // never returns anything, crash break your script
+  throw { message, code }
+}
+
+error('An error occurred!', 500)
