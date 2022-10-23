0d553ce6f3eaa078396d19cd7c2ac5d5cc270e1c
Miguel <miguel.castillo-melendez81@bcmail.cuny.edu>
2022-10-23 19:35:06 -0400

    added more generic function example.

--- dist/app.js
+++ dist/app.js
@@ -3,3 +3,12 @@ function merge(objA, objB) {
     return Object.assign(Object.assign({}, objA), objB);
 }
 const mergedObj = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
+function countAndDescribe(element) {
+    let desc = 'Got no value';
+    if (element.length === 1)
+        desc = `Got ${element.length} element.`;
+    else if (element.length > 1)
+        desc = `Got ${element.length} elements.`;
+    return [element, desc];
+}
+console.log(countAndDescribe(['Sports', 'Cooking']));
--- src/app.ts
+++ src/app.ts
@@ -4,3 +4,19 @@ function merge<T extends object, U extends object>(objA: T, objB: U) {
 }
 
 const mergedObj = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 30 })
+
+// more generic functions
+interface Lengthy {
+  length: number
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let desc = 'Got no value'
+
+  if (element.length === 1) desc = `Got ${element.length} element.`
+  else if (element.length > 1) desc = `Got ${element.length} elements.`
+
+  return [element, desc]
+}
+
+console.log(countAndDescribe(['Sports', 'Cooking']))
