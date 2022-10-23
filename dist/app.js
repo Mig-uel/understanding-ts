8a075b23e4f72189dbd27518b0f3767f45e1ffa1
Miguel <miguel.castillo-melendez81@bcmail.cuny.edu>
2022-10-23 19:53:36 -0400

    generic classes.

--- dist/app.js
+++ dist/app.js
@@ -16,3 +16,30 @@ function extractAndConvert(obj, key) {
     return obj[key];
 }
 extractAndConvert({ name: 'Max' }, 'name');
+class DataStorage {
+    constructor() {
+        this.data = [];
+    }
+    addItem(item) {
+        this.data.push(item);
+    }
+    removeItem(item) {
+        if (this.data.indexOf(item) === -1)
+            return;
+        this.data.splice(this.data.indexOf(item), 1);
+    }
+    getItems() {
+        return [...this.data];
+    }
+}
+const textStorage = new DataStorage();
+textStorage.addItem('Max');
+textStorage.addItem('Manu');
+textStorage.removeItem('Max');
+console.log(textStorage.getItems());
+const numberStorage = new DataStorage();
+const objStorage = new DataStorage();
+objStorage.addItem({ name: 'Max' });
+objStorage.addItem({ name: 'Manu' });
+objStorage.removeItem({ name: 'Manu' });
+console.log(objStorage.getItems());
--- src/app.ts
+++ src/app.ts
@@ -30,3 +30,37 @@ function extractAndConvert<T extends object, U extends keyof T>(
 }
 
 extractAndConvert({ name: 'Max' }, 'name')
+
+// generic classes
+class DataStorage<T> {
+  private data: T[] = []
+
+  addItem(item: T) {
+    this.data.push(item)
+  }
+
+  removeItem(item: T) {
+    if (this.data.indexOf(item) === -1) return
+
+    this.data.splice(this.data.indexOf(item), 1)
+  }
+
+  getItems() {
+    return [...this.data]
+  }
+}
+
+const textStorage = new DataStorage<string>() // textStorage will only be of type string
+// textStorage.addItem(10) -> error trying to add number to a text type
+textStorage.addItem('Max')
+textStorage.addItem('Manu')
+textStorage.removeItem('Max')
+console.log(textStorage.getItems())
+
+const numberStorage = new DataStorage<number>() // numberStorage will be of type number
+
+const objStorage = new DataStorage<object>()
+objStorage.addItem({ name: 'Max' })
+objStorage.addItem({ name: 'Manu' })
+objStorage.removeItem({ name: 'Manu' })
+console.log(objStorage.getItems())
