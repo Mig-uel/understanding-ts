b2cb6693a334bcba280b48b948bb59264d764486
Miguel <miguel.castillo-melendez81@bcmail.cuny.edu>
2022-10-24 17:15:28 -0400

    accessor, method and parameter decorators.

--- src/app.ts
+++ src/app.ts
@@ -46,6 +46,34 @@ function Log(target: any, propertyName: string | Symbol) {
   console.log(target, propertyName)
 }
 
+// accessor decorator
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+  console.log('---- Accessor Decorator ----')
+  console.log(target)
+  console.log(name)
+  console.log(descriptor)
+}
+
+// method decorator
+function Log3(
+  target: any,
+  name: string | Symbol,
+  descriptor: PropertyDescriptor
+) {
+  console.log('---- Method Decorator ----')
+  console.log(target)
+  console.log(name)
+  console.log(descriptor)
+}
+
+// parameter decorator
+function Log4(target: any, name: string | Symbol, position: number) {
+  console.log('---- Parameter Decorator ----')
+  console.log(target)
+  console.log(name)
+  console.log(position)
+}
+
 class Product {
   @Log
   title: string
@@ -56,12 +84,14 @@ class Product {
     this._price = _price
   }
 
+  @Log2
   set price(val: number) {
     if (val > 0) this._price = val
     else throw new Error('Invalid price!')
   }
 
-  getPriceWithTax(tax: number) {
+  @Log3
+  getPriceWithTax(@Log4 tax: number) {
     return this._price * (1 + tax)
   }
 }
