643389cdaca619c88ec606439fd3278947457b5d
Miguel <miguel.castillo-melendez81@bcmail.cuny.edu>
2022-10-23 20:44:16 -0400

    another way of creating decorators: decorator factories.

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,23 @@
 // decorators
-function Logger(constructor: Function) {
-  console.log('Logging...')
-  console.log(constructor)
+
+/*
+  function Logger(constructor: Function) {
+    console.log('LOGGING...')
+    console.log(constructor)
+  }
+
+  @Logger
+*/
+
+// Decorator Factory
+function Logger(logString: string) {
+  return function (constructor: Function) {
+    console.log(logString)
+    console.log(constructor)
+  }
 }
 
-@Logger
+@Logger('LOGGING - PERSON') // call decorator
 class Person {
   name = 'Max'
 
