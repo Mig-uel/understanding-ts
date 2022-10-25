994fbd535832900afdf73e6d297282ac377dab86
Miguel <miguel.castillo-melendez81@bcmail.cuny.edu>
2022-10-25 13:42:46 -0400

    creating an autobind decorator.

--- src/app.ts
+++ src/app.ts
@@ -105,4 +105,33 @@ class Product {
   }
 }
 
-// returning and changing a class decorator
+function AutoBind(
+  _: any,
+  __: string,
+  descriptor: PropertyDescriptor
+): PropertyDescriptor {
+  const originalMethod = descriptor.value
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    enumerable: false,
+    get() {
+      const boundFn = originalMethod.bind(this)
+      return boundFn
+    },
+  }
+  return adjDescriptor
+}
+
+class Printer {
+  private message = 'This works!'
+
+  @AutoBind
+  showMessage() {
+    console.log(this.message)
+  }
+}
+
+const p1 = new Printer()
+
+const button = document.querySelector('button') as HTMLButtonElement
+button.addEventListener('click', p1.showMessage)
