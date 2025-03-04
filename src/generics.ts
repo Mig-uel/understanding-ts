21b387667bec67554eec1b65a654c1f596b6eb25
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-03-04 14:56:20 -0500

    Add mergeObj function for object merging with generics
    - Introduced mergeObj function to merge two objects
    - Enhanced type safety by constraining types to objects
    - Updated console logs for better output clarity

--- src/generics.ts
+++ src/generics.ts
@@ -1,3 +1,5 @@
+// Generics
+
 let names: Array<string> = ['max', 'anna']
 
 type DataStore<T> = {
@@ -14,6 +16,15 @@ function merge<T, U>(a: T, b: U) {
 
 const [a, b] = merge(1, 'two')
 
-console.log(val)
+console.log(a, b)
+
+// Generics & Constraints
+
+function mergeObj<T extends object>(a: T, b: T) {
+  return { ...a, ...b }
+}
+
+const merged = mergeObj({ username: 'max' }, { age: 12 })
+console.log(merged)
 
 export {}
