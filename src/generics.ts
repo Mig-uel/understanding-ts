909dbf33d25472c1440189faddfba9521baf523c
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-03-04 14:50:46 -0500

    Update merge function to support different types - Modify merge function to accept two parameters of different types - Enhance type flexibility for better usage in generics

--- src/generics.ts
+++ src/generics.ts
@@ -8,11 +8,11 @@ let store: DataStore<string | boolean> = {}
 store.name = 'max'
 store.instructor = true
 
-function merge<T>(a: T, b: T) {
+function merge<T, U>(a: T, b: U) {
   return [a, b]
 }
 
-const [a, b] = merge(1, 2)
+const [a, b] = merge(1, 'two')
 
 console.log(val)
 
