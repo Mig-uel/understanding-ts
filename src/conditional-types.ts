28016366b01df84db563a08d924466dc6ad35404
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-03-04 16:51:07 -0500

    Add FullNameOrNothing type and getFullName function
    - Introduces FullNameOrNothing type to conditionally return a string
      based on the presence of firstName and lastName properties.
    - Implements getFullName function to construct a full name
      from a person object or throw an error if names are missing.

--- src/conditional-types.ts
+++ src/conditional-types.ts
@@ -9,4 +9,23 @@ type ElementType<T> = T extends any[] ? T[number] : never
 type Example1 = ElementType<StringArray>
 type Example2 = ElementType<typeof text>
 
+type FullnamePerson = {
+  firstName: string
+  lastName: string
+}
+type FullNameOrNothing<T> = T extends FullnamePerson ? string : never
+
+function getFullName<T extends object>(person: T): FullNameOrNothing<T> {
+  if (
+    'firstName' in person &&
+    'lastName' in person &&
+    person.firstName &&
+    person.lastName
+  ) {
+    return `${person.firstName} ${person.lastName}` as FullNameOrNothing<T>
+  }
+
+  throw new Error('No first name and/or last name found.')
+}
+
 export {}
