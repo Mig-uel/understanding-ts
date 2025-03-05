5612fb9dcd11a6b9a3fbfe2c9c442540b3a0630f
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-03-05 04:33:04 -0500

    Remove unused distribution files and update .gitignore
    - Deleted all files in the 'dist' directory as they are no longer needed.
    - Updated .gitignore to exclude the 'dist' folder from version control.

--- dist/basics.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function addNums(n1, n2, printResult, phrase) {
-    printResult ? console.log(phrase, n1 + n2) : n1 + n2;
-}
-const number1 = 5;
-const number2 = 2.8;
-const println = true;
-const phrase = 'Result is:';
-addNums(number1, number2, println, phrase);
