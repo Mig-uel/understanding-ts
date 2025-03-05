5d122fdb025d0059947dc182a741f679db4d2608
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-03-05 05:01:01 -0500

    Enhance Logger decorator to return a new class instance
    - Updated Logger decorator to merge original class with new properties
    - Improved type safety by using generics for the target class

--- src/decorators/index.ts
+++ src/decorators/index.ts
@@ -16,10 +16,19 @@
  *
  */
 
-function Logger(target: any, context: ClassDecoratorContext) {
+function Logger<T extends new (...args: any[]) => any>(
+  target: T,
+  context: ClassDecoratorContext
+) {
   console.log('Logger Decorator')
   console.log(target) // class Person
   console.log(context) // {kind, name, metadata, addInitializer}
+
+  // returning a new class
+  return class extends target {
+    // original class does not get replaced but merged
+    age = 35
+  }
 }
 
 @Logger
@@ -30,5 +39,8 @@ class Person {
     console.log(`Hi, I am ${this.name}`)
   }
 }
+
+const max = new Person()
+console.log(max)
 
 export {}
