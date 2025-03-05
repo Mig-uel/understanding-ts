49d2ef606fe15a5ad4288064cbc299dbd1c5577a
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-03-05 04:50:07 -0500

    Add decorators implementation and documentation
    - Introduced a new file for decorators in TypeScript
    - Provided an overview of decorator types and their usage

---
+++ src/decorators/index.ts
@@ -0,0 +1,17 @@
+/**
+ * ---- DECORATORS ----
+ *
+ * Decorators are a meta-programming feature; code that interacts with other code.
+ *
+ * TypeScript supports two kinds of decorators:
+ * - ECMAScript (built-into JavaScript)
+ * - Experimental Decorators
+ *
+ * Types of Decorators:
+ * - Class Decorators
+ * - Method Decorators
+ * - Field Decorators
+ * - Getter Decorators 
+ * - Setter Decorators
+ *
+ */
